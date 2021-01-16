949434caee6d40df8192a918e72727cae34c159d
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-01-16 15:32:25 -0700

    Initial commit

---
+++ Source/DangerousGame/DangerousGameMod.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using UnityEngine;
+
+
+namespace DangerousGame
+{
+    public class DangerousGameMod : Mod
+    {
+        public DangerousGameSettings settings;
+        public static DangerousGameMod mod;
+        public override string SettingsCategory()
+        {
+            return "ZDG_ModName".Translate();
+        }
+
+        public DangerousGameMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<DangerousGameSettings>();
+            mod = this;
+        }
+
+        public override void DoSettingsWindowContents(UnityEngine.Rect inRect)
+        {
+            Listing_Standard mainListing = new Listing_Standard();
+            mainListing.Begin(inRect);
+            DangerousGameSettings settings = DangerousGameMod.mod.settings;
+
+            //Text.Font = GameFont.Medium;
+            //GUI.color = new Color(255, 180, 0);
+            //mainListing.Label("ZDG_Restart".Translate());
+
+            //GUI.color = Color.white;
+            //Text.Font = GameFont.Small;
+            //mainListing.Gap();
+
+            // Valid Prey
+            mainListing.Label("ZDG_ValidPrey".Translate());
+            Rect preyOptionsRect = mainListing.GetRect(30f);
+            Rect preySelectRect = preyOptionsRect.LeftPart(0.19f).Rounded();
+            Rect preySelectDescRect = preyOptionsRect.RightPart(0.79f).Rounded();
+
+            Listing_Standard preyListing = new Listing_Standard();
+            preyListing.Begin(preySelectRect);
+
+            if (preyListing.ButtonText(("ZDG_ValidPrey" + (int)settings.validPrey).Translate()))
+            {
+                List<FloatMenuOption> preyOptionsList = new List<FloatMenuOption>();
+                foreach (ValidPrey prey in Enum.GetValues(typeof(ValidPrey)))
+                {
+                    preyOptionsList.Add(new FloatMenuOption(("ZDG_ValidPrey" + (int)prey).Translate(), delegate { settings.validPrey = prey; }, MenuOptionPriority.Default));
+                }
+                Find.WindowStack.Add(new FloatMenu(preyOptionsList));
+            }
+            preyListing.End();
+            Widgets.Label(preySelectDescRect, ("ZDG_ValidPreyDesc" + (int)settings.validPrey).Translate());
+            mainListing.Gap();
+
+
+            // Predator types
+            mainListing.Label("ZDG_PredType".Translate());
+
+            Rect predOptionsRect = mainListing.GetRect(30f);
+            Rect predSelectRect = predOptionsRect.LeftPart(0.19f).Rounded();
+            Rect predSelectDescRect = predOptionsRect.RightPart(0.79f).Rounded();
+
+            Listing_Standard predTypeListing = new Listing_Standard();
+            predTypeListing.Begin(predSelectRect);
+            if (predTypeListing.ButtonText(("ZDG_PredType" + (int)settings.predType).Translate()))
+            {
+                List<FloatMenuOption> preyOptionsList = new List<FloatMenuOption>();
+                foreach (PredType predType in Enum.GetValues(typeof(PredType)))
+                {
+                    preyOptionsList.Add(new FloatMenuOption(("ZDG_PredType" + (int)predType).Translate(), delegate { settings.predType = predType; }, MenuOptionPriority.Default));
+                }
+                Find.WindowStack.Add(new FloatMenu(preyOptionsList));
+            }
+            predTypeListing.End();
+            Widgets.Label(predSelectDescRect, ("ZDG_PredTypeDesc" + (int)settings.predType).Translate());
+
+            mainListing.Gap();
+
+            // revenge chance
+            Texture2D harmIcon = ContentFinder<Texture2D>.Get("UI/Designators/Hunt");
+            Texture2D tameIcon = ContentFinder<Texture2D>.Get("UI/Designators/Tame");
+
+            Rect harmLabelRect = mainListing.GetRect(Text.LineHeight);
+
+            WidgetRow harmWR = new WidgetRow();
+            harmWR.Init(harmLabelRect.xMin, harmLabelRect.yMin);
+            harmWR.Icon(harmIcon);
+            harmWR.Label("HarmedRevengeChance".Translate());
+            mainListing.IntRange(ref settings.revOnDamage, 0, 100);
+            mainListing.Gap();
+
+            Rect tameLabelRect = mainListing.GetRect(Text.LineHeight);
+            WidgetRow tameWR = new WidgetRow();
+            tameWR.Init(tameLabelRect.xMin, tameLabelRect.yMin);
+            tameWR.Icon(tameIcon);
+            tameWR.Label("TameFailedRevengeChance".Translate());
+            mainListing.IntRange(ref settings.revOnTameFail, 0, 100);
+            mainListing.Gap();
+
+            Text.Font = GameFont.Medium;
+            GUI.color = new Color(255, 180, 0);
+            mainListing.Label("ZDG_Restart".Translate());
+
+            GUI.color = Color.white;
+            Text.Font = GameFont.Small;
+
+            mainListing.End();
+
+        }
+
+    }
+
+
+
+    public class DangerousGameSettings : ModSettings
+    {
+        public static bool spawnHungry = true;
+        public ValidPrey validPrey = ValidPrey.Predators;
+        public PredType predType = PredType.MeatEaters;
+        public IntRange revOnDamage = new IntRange(50, 100);
+        public IntRange revOnTameFail = new IntRange(50, 100);
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref spawnHungry, "spawnHungry", true);
+            Scribe_Values.Look(ref validPrey, "validPrey", ValidPrey.Predators);
+            Scribe_Values.Look(ref predType, "predType", PredType.MeatEaters);
+            Scribe_Values.Look(ref revOnDamage, "revOnDamage", new IntRange(20, 100));
+            Scribe_Values.Look(ref revOnTameFail, "revOnTameFail", new IntRange(20, 100));
+
+        }
+    }
+
+    public enum ValidPrey : byte
+    {
+        Vanilla,
+        Predators,
+        Humans,
+        Colonists
+    }
+
+    public enum PredType : byte
+    {
+        Vanilla,
+        MeatEaters,
+        Dangerous,
+        Everything
+    }
+}
---
+++ Source/DangerousGame/HarmonyPatches.cs
@@ -0,0 +1,127 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace DangerousGame
+{
+    public static class HarmonyPatches
+    {
+        [StaticConstructorOnStartup]
+        public static class DangerousGame
+        {
+            static DangerousGame()
+            {
+                Log.Message("More Dangerous Game initializing...");
+
+                new Harmony("zylle.DangerousGame").PatchAll();
+                ModifyAnimals();
+            }
+
+            static void ModifyAnimals()
+            {
+                DangerousGameSettings settings = DangerousGameMod.mod.settings;
+                float revOnHarmMin = 0.01f * (float)settings.revOnDamage.min;
+                float revOnHarmMax = 0.01f * (float)settings.revOnDamage.max;
+                float revOnTameMin = 0.01f * (float)settings.revOnTameFail.min;
+                float revOnTameMax = 0.01f * (float)settings.revOnTameFail.max;
+
+                foreach (PawnKindDef pawn in DefDatabase<PawnKindDef>.AllDefs)
+                {
+                    if (pawn.RaceProps.Animal)
+                    {
+                        // set predator types
+                        switch(settings.predType)
+                        {
+                            case PredType.Everything:
+                                pawn.RaceProps.predator = true;
+                                break;
+                            case PredType.MeatEaters:
+                                if (pawn.RaceProps.Eats(FoodTypeFlags.Corpse))
+                                {
+                                    pawn.RaceProps.predator = true;
+                                }
+                                break;
+                            case PredType.Dangerous:
+                                if (pawn.combatPower >= 100f)
+                                {
+                                    pawn.RaceProps.predator = true;
+                                }
+                                break;
+
+                            default:        // default covers vanilla
+                                break;
+                        }
+
+
+                        // revenge chance
+                        pawn.RaceProps.manhunterOnDamageChance = Math.Min(revOnHarmMax, Math.Max(revOnHarmMin, pawn.RaceProps.manhunterOnDamageChance));
+                        pawn.RaceProps.manhunterOnTameFailChance = Math.Min(revOnTameMax, Math.Max(revOnTameMin, pawn.RaceProps.manhunterOnDamageChance));
+
+                    }
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(RimWorld.FoodUtility), "IsAcceptablePreyFor")]
+    static class PredatorPreyPatch
+    {
+        static bool Prefix(Pawn predator, Pawn prey, ref bool __result)
+        {
+            // Vanilla
+            if (DangerousGameMod.mod.settings.validPrey == ValidPrey.Vanilla)
+            {
+                return true;
+            }
+
+            // Predators are not prey
+            if (DangerousGameMod.mod.settings.validPrey == ValidPrey.Predators)
+            {
+                if (prey.RaceProps.predator && !prey.RaceProps.Humanlike)
+                {
+                    __result = false;
+                    return false;
+                }
+            }
+
+            // Wild predators only hunt humans. Tame predators don't hunt other predators.
+            if (DangerousGameMod.mod.settings.validPrey == ValidPrey.Humans)
+            {
+                if (predator.Faction == null)
+                {
+                    __result = false;
+                    if (prey.RaceProps.Humanlike)
+                    {
+                        __result = true;
+                    }
+                    return false;
+                }
+                else if (prey.RaceProps.predator && !prey.RaceProps.Humanlike)
+                {
+                    __result = false;
+                    return false;
+                }
+            }
+
+            // All predators hunt only colonists
+            if (DangerousGameMod.mod.settings.validPrey == ValidPrey.Colonists)
+            {
+                __result = false;
+                if (prey.RaceProps.Humanlike && prey.Faction == Faction.OfPlayer)
+                {
+                    __result = true;
+                }
+                return false;
+            }
+
+
+            return true;
+        }
+    }
+
+}
